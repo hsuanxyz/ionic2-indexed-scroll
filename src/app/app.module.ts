28d8ce8e249c987e2bbd28a8c55e93412c079044
HsuanXyz <HsuanGM@gmail.com>
2017-04-10 14:17:03 +0800

    Update ionic to 3.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
@@ -17,6 +19,8 @@ import { Contacts } from  '../providers/contacts'
     TabsPage
   ],
   imports: [
+    HttpModule,
+    BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
